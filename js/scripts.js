da9e8dc696a906d4c3461f9b9bf9f85cf02cce77
Bob3x <bob3xx@protonmail.com>
2024-08-10 16:13:42 +0300

    Crossing an item function

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// Main function 
+function newItem() {
+
+    // Adding a new item to the list
+    let li = $("<li></li>");
+    let inputValue = $("#input").val();
+    li.append(inputValue);
+
+    if (inputValue === "") {
+        alert("You must write something!");
+    }else {
+        $("#list").append(li);
+    }
+
+    // Crossing an item from the list adding class strike in CSS
+    function crossOut() {
+        li.toggleClass("strike");
+    }
+    li.on("click", crossOut);
+
+
+}
